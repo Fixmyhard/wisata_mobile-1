5746653501aa230a862f224459aec8e9646b5231
Darius <dariusdarren86@gmail.com>
2024-10-22 15:02:39 +0700

    flutter mobile splash screen pertemuan ke 4

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:aslab_travel/screens/SplashScreen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(TravelApp());
+}
+
+class TravelApp extends StatelessWidget {
+  const TravelApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
+
+class MyWidget extends StatefulWidget {
+  const MyWidget({super.key});
+
+  @override
+  State<MyWidget> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
